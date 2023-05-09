7ab409ae918aa3168097c22918c343ecbd34234e
Chesterjakee <56465026+Chesterjakee@users.noreply.github.com>
2023-05-09 21:01:07 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,13 @@ from urllib.error import URLError
 streamlit.title('HAPPY 2ND MONTHSARY')
 streamlit.header(' BABY BOYFRIEND ASAWA')
 streamlit.text('Hi baby, boyfriend, my asawa. Happy monthsary to the both of us. Baby ko, \nI love you very very very very much. There\'s no ruler or a measuring \ntool that can measure this love that I\'m feeling for you. You are a \ntreasure that I will always value. Thank you for being with me always. \nYou know how lucky I am that God gave me a kind of person that \nunderstands, loves and proud of who I am. Baby ko, I hope that you\'ll \nalways there for me not only in happiness but also in sadness, whatever \nseason there is although 2 seasons lang naman tayo. Baby kopo, I\'m \nalways here for you po, support on the things that you\'ll do that will \nbenefit you of course. ')
+streamlit.markdown(#)
+streamlit.text('\nBaby po, thank you for all the efforts that you made for me, all of that \nare not wasted and I won\'t waste it. I think you already knew that you \nare the biggest factor why my mood is so lively and I can put smile on \nmy face always, thank you for being the reason behind it. I\'ll do \neverything to make you smile the way you made me smile.  ')
+streamlit.markdown(#)
+streamlit.text('\nI\'m thankful to Him because he gave you to me, I won\'t waste any moment \nwith you. Baby ko, I love you so much. There are lines in Taylor swift \nsong that are so simple yet describes the way I feel for you.')
+streamlit.markdown(#)
+streamlit.text('\nI hope I\'ll never lose you, I hope this never ends; Baby ko, I\'m hoping \nthat I won\'t lose you, i\'ll do everything to never end this. Can I go \nwhere you go? Can we always be this close? These are the lines that \nalways circles my mind whenever we\'re together; Oh I wish that I can go \nwherever you go and always be close to you that I can feel how your \nheart beats. I love you so much. Lastly, I once believe love would be \nburning red but its not, its daylight; Just like the ray of sun, my love \nfor you is daylight, it constantly shines and it shines beautifully. \nIt\'s also golden, which is less than the love I can offer to you.')
+streamlit.markdown(#)
+streamlit.text('\nBaby po, I love you so much. Mahal na mahal na mahal kita sobra sobra \nbaby asawa ko po, boyfriend ko. Akin kalang lagi ah? Ipaglalaban kopo \npalagi kung anong meron tayo. You are so worth it baby ko. I love you so \nso much baby Necart ko.')
 
 
