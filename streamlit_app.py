f1a0e4a5851f5c6dca0005d3dddf115756f76512
Chesterjakee <56465026+Chesterjakee@users.noreply.github.com>
2023-05-09 20:35:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,7 @@ from urllib.error import URLError
 
 streamlit.title('HAPPY 2ND MONTHSARY')
 streamlit.header(' BABY BOYFRIEND ASAWA')
-streamlit.text('Hi baby, boyfriend, my asawa. Happy monthsary to the both of us. Baby ko, I love you very very very very much. There\'s no ruler or a measuring tool that can measure this love that I\'m feeling for you. You are a treasure that I will always value. Thank you for being with me always. You know how lucky I am that God gave me a kind of person that understands, loves and proud of who I am. Baby ko, I hope that you\'ll always there for me not only in happiness but also in sadness, whatever season there is although 2 seasons lang naman tayo. Baby kopo, I\'m always here for you po, support on the things that you\'ll do that will benefit you of course. 
-')
+streamlit.text('Hi baby, boyfriend, my asawa. Happy monthsary to the both of us. Baby ko, I love you very very very very much. There\'s no ruler or a measuring tool that can measure this love that I\'m feeling for you. You are a treasure that I will always value. Thank you for being with me always. You know how lucky I am that God gave me a kind of person that understands, loves and proud of who I am. Baby ko, I hope that you\'ll always there for me not only in happiness but also in sadness, whatever season there is although 2 seasons lang naman tayo. Baby kopo, I\'m always here for you po, support on the things that you\'ll do that will benefit you of course.')
 
 '''
 streamlit.title('My Parents New Healthy Diner')
