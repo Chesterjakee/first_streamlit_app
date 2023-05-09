94c2cb5935db7c02e26414535ac9d5dcb5ff5b09
Chesterjakee <56465026+Chesterjakee@users.noreply.github.com>
2023-05-09 20:57:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,6 @@ from docx.shared
 
 streamlit.title('HAPPY 2ND MONTHSARY')
 streamlit.header(' BABY BOYFRIEND ASAWA')
-doc.add_paragraph('Hi baby, boyfriend, my asawa. Happy monthsary to the both of us. \nBaby ko, I love you very very very very much. There\'s no ruler or a measuring tool that can measure \nthis love that I\'m feeling for you. You are a treasure that I will always value. \nThank you for being with me always. You know how lucky I am that God gave me a kind of person that understands, \nloves and proud of who I am. Baby ko, I hope that you\'ll always there for me not only in happiness but also in \nsadness, whatever season there is although 2 seasons lang naman tayo. Baby kopo, I\'m always here for you po, \nsupport on the things that you\'ll do that will benefit you of course.')
+streamlit.text('Hi baby, boyfriend, my asawa. Happy monthsary to the both of us. Baby ko, \nI love you very very very very much. There\'s no ruler or a measuring \ntool that can measure this love that I\'m feeling for you. You are a \ntreasure that I will always value. Thank you for being with me always. \nYou know how lucky I am that God gave me a kind of person that \nunderstands, loves and proud of who I am. Baby ko, I hope that you\'ll \nalways there for me not only in happiness but also in sadness, whatever \nseason there is although 2 seasons lang naman tayo. Baby kopo, I\'m \nalways here for you po, support on the things that you\'ll do that will \nbenefit you of course. ')
 
 
