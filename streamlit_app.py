639f6a01e37a57765934fd541d46b1bcc5f6c9e0
Chesterjakee <56465026+Chesterjakee@users.noreply.github.com>
2023-05-09 21:03:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-streamlit.title('❤️❤️HAPPY 2ND MONTHSARY❤️❤️')
+streamlit.title('❤️HAPPY 2ND MONTHSARY❤️')
 streamlit.header(' ❤️❤️❤️ BABY BOYFRIEND ASAWA ❤️❤️❤️')
 streamlit.text('Hi baby, boyfriend, my asawa. Happy monthsary to the both of us. Baby ko, \nI love you very very very very much ❤️❤️❤️. There\'s no ruler or a measuring \ntool that can measure this love that I\'m feeling for you. You are a \ntreasure that I will always value. Thank you for being with me always. \nYou know how lucky I am that God gave me a kind of person that \nunderstands, loves and proud of who I am. Baby ko, I hope that you\'ll \nalways there for me not only in happiness but also in sadness, whatever \nseason there is although 2 seasons lang naman tayo. Baby kopo, I\'m \nalways here for you po, support on the things that you\'ll do that will \nbenefit you of course. ')
 streamlit.markdown('#')
